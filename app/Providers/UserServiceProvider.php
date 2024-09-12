97bb2e72812da6510c8b673dfba39170338716e0
Yutsss <adyuta123@gmail.com>
2024-09-12 12:43:51 +0700

    checkpoint 2

---
+++ app/Providers/UserServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Providers;
+
+use App\Services\impl\UserServiceImpl;
+use App\Services\UserService;
+use Illuminate\Contracts\Support\DeferrableProvider;
+use Illuminate\Support\ServiceProvider;
+
+class UserServiceProvider extends ServiceProvider implements DeferrableProvider
+{
+    public array $singletons = [
+        UserService::class => UserServiceImpl::class
+    ];
+
+
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+    public function provides(): array
+    {
+        return [
+            UserService::class
+        ];
+    }
+}
---
+++ app/Services/impl/UserServiceImpl.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Services\impl;
+
+use App\Services\UserService;
+
+class UserServiceImpl implements UserService
+{
+
+}
